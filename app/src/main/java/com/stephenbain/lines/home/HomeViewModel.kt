65758d1932cfcb76a7ba7e358149b25a926cf8dc
Stephen Bain <swbain@gmail.com>
2020-08-09 22:42:24 -0500

    adding io dispatcher

--- app/src/main/java/com/stephenbain/lines/home/HomeViewModel.kt
+++ app/src/main/java/com/stephenbain/lines/home/HomeViewModel.kt
@@ -9,9 +9,12 @@ import androidx.paging.map
 import com.stephenbain.lines.repository.CategoryItem
 import com.stephenbain.lines.repository.GetLatestTopicsRepository
 import com.stephenbain.lines.repository.TopicWithUsersAndCategory
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.observeOn
 
 @FlowPreview
 @ExperimentalCoroutinesApi
@@ -22,7 +25,7 @@ class HomeViewModel @ViewModelInject constructor(
 
     val data = topicsRepository.getTopics(CategoryItem.AllCategories)
         .map { createUiModel(it) }
-        .asLiveData(viewModelScope.coroutineContext)
+        .asLiveData(viewModelScope.coroutineContext + Dispatchers.IO)
 
     private fun createUiModel(pagingData: PagingData<TopicWithUsersAndCategory>): PagingData<TopicCardUiModel> {
         return pagingData.map { topicToUiModel(it) }
